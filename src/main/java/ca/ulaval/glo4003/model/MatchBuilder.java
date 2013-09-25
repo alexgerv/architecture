af8c5440caae44c957a5513a9a2d08551292bc3f
Kevin Dion <kevin.dion.1@ulaval.ca>
2013-09-24 23:40:09 -0400

    Attempt to make the matchBuilder easy to maintain

--- src/main/java/ca/ulaval/glo4003/model/MatchBuilder.java
+++ src/main/java/ca/ulaval/glo4003/model/MatchBuilder.java
@@ -1,67 +1,91 @@
 package ca.ulaval.glo4003.model;
 
-import java.lang.reflect.Field;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MatchBuilder {
 
-    private String sport;
-    private Date date;
-    private String venue;
-    private String homeTeam;
-    private String visitorTeam;
-    private Map<String, Integer> avalaibleTicketsBySection = new HashMap<String, Integer>();
+    private static final String SPORT_KEY = "Sport";
+    private static final String DATE_KEY = "Date";
+    private static final String VENUE_KEY = "Venue";
+    private static final String HOME_TEAM_KEY = "HomeTeam";
+    private static final String VISITOR_TEAM_KEY = "VisitorTeam";
+    private static final String AVAILABLE_SECTIONS_KEY = "AvailableSections";
 
-    public Match build() {
-        validateState();
-        return new Match(sport, venue, date, homeTeam, visitorTeam, avalaibleTicketsBySection);
+    private Map<String, Object> matchAttributes = new HashMap<String, Object>();
+
+    public MatchBuilder() {
+        this.setupMatchAttributes();
     }
 
-    private void validateState() {
-        for (Field field : MatchBuilder.class.getDeclaredFields()) {
-            try {
-                if (field.get(this) == null) {
-                    throw new MatchBuilderException(field.getName());
-                }
-            } catch (IllegalArgumentException | IllegalAccessException e) {
-                e.printStackTrace(); // Decider de ce qu'on fait avec �a
+    private void setupMatchAttributes() {
+        this.matchAttributes.put(SPORT_KEY, null);
+        this.matchAttributes.put(DATE_KEY, null);
+        this.matchAttributes.put(VENUE_KEY, null);
+        this.matchAttributes.put(HOME_TEAM_KEY, null);
+        this.matchAttributes.put(VISITOR_TEAM_KEY, null);
+        this.matchAttributes.put(AVAILABLE_SECTIONS_KEY, null);
+    }
+
+    public Match build() {
+        this.validateAttributes();
+        Match newMatch = this.buildMatch();
+        this.setupMatchAttributes();
+        return newMatch;
+    }
+
+    private void validateAttributes() {
+        for (String attributeName : this.matchAttributes.keySet()) {
+            if (this.matchAttributes.get(attributeName) == null) {
+                throw new MatchBuilderException(attributeName);
             }
         }
-        if(avalaibleTicketsBySection.isEmpty()) {
-        	throw new MatchBuilderException("avalaibleTicketsBySection");
-        }
+    }
 
+    private Match buildMatch() {
+        Match newMatch = new Match((String) (matchAttributes.get(SPORT_KEY)),
+                                   (String) (matchAttributes.get(VENUE_KEY)),
+                                   (Date) (matchAttributes.get(DATE_KEY)),
+                                   (String) (matchAttributes.get(HOME_TEAM_KEY)),
+                                   (String) (matchAttributes.get(VISITOR_TEAM_KEY)),
+                                   (Map<String, Integer>) (matchAttributes.get(AVAILABLE_SECTIONS_KEY)));
+        return newMatch;
     }
 
     public MatchBuilder createSection(String sectionsName, int numberOfTickets) {
-        avalaibleTicketsBySection.put(sectionsName, numberOfTickets);
+        if (this.matchAttributes.get(AVAILABLE_SECTIONS_KEY) == null) {
+            Map<String, Integer> section = new HashMap<String, Integer>();
+            section.put(sectionsName, numberOfTickets);
+            this.matchAttributes.put(AVAILABLE_SECTIONS_KEY, section);
+        } else {
+            ((Map<String, Integer>) this.matchAttributes.get(AVAILABLE_SECTIONS_KEY)).put(sectionsName, numberOfTickets);
+        }
         return this;
     }
 
     public MatchBuilder setSport(String sport) {
-        this.sport = sport;
+        this.matchAttributes.put(SPORT_KEY, sport);
         return this;
     }
 
     public MatchBuilder setDate(Date date) {
-        this.date = date;
+        this.matchAttributes.put(DATE_KEY, date);
         return this;
     }
 
     public MatchBuilder setVenue(String venue) {
-        this.venue = venue;
+        this.matchAttributes.put(VENUE_KEY, venue);
         return this;
     }
 
     public MatchBuilder setHomeTeam(String homeTeam) {
-        this.homeTeam = homeTeam;
+        this.matchAttributes.put(HOME_TEAM_KEY, homeTeam);
         return this;
     }
 
     public MatchBuilder setVisitorTeam(String visitorTeam) {
-        this.visitorTeam = visitorTeam;
+        this.matchAttributes.put(VISITOR_TEAM_KEY, visitorTeam);
         return this;
     }
 }
