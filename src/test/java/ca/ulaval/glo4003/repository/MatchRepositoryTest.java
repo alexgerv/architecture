c4d4fc3293cbfdd5e17a61e6303d5ce7d7b03bd1
Kevin Dion <kevin.dion.1@ulaval.ca>
2013-10-07 01:15:32 -0400

    Added tests for the MatchRepository

--- src/test/java/ca/ulaval/glo4003/repository/MatchRepositoryTest.java
+++ src/test/java/ca/ulaval/glo4003/repository/MatchRepositoryTest.java
@@ -1,35 +1,35 @@
 package ca.ulaval.glo4003.repository;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verifyZeroInteractions;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
-import java.util.Map;
+import java.io.PrintStream;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import ca.ulaval.glo4003.fileAccess.FileAccessor;
 import ca.ulaval.glo4003.fileAccess.JSONMatchConverter;
 import ca.ulaval.glo4003.model.Match;
 
 public class MatchRepositoryTest {
 
-    private static final String[] INVALID_FILES_NAME_IN_A_DIRECTORY = { "InvalidFileA.json", "InvalidFileB.json" };
-    private static final String[] VALID_FILES_NAME_IN_A_DIRECTORY = { "ValidFileA.json", "ValidFileB.json" };
-    private static final String FILE_NOT_FOUND_EXCEPTION_MESSAGE =
-                                                                   "whenRootDirectoryIsNotValidNoEntriesAreAddedToTheMatchRepository: FileNotFoundException has been thrown.";
     private static final Integer[] VALID_MATCH_INDEXES = {0, 1, 2, 3, 4,};
+    private static final Integer[] INVALID_MATCH_INDEXES = {5};
+    private static final String AN_ERROR_MESSAGE = "Error";
     private static final String ROOT_REPOSITORY = "./matches/";
 
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    
     private MatchRepository aMatchRepository;
 
     @Mock
@@ -40,51 +40,42 @@ public class MatchRepositoryTest {
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
+        System.setErr(new PrintStream(errContent));
         aMatchRepository = new MatchRepository(JSONMatchConverter);
+    }
+    
+    @After
+    public void cleanup(){
+        System.setOut(null);
     }
     
     @Test 
     public void whenGettingAListOfMatchesIfAMatchIsNotLoadedThenItIsLoaded() throws FileNotFoundException{
-        aMatchRepository.getMatchesByIndex(VALID_MATCH_INDEXES);
         String expectedPathOfFileToLoad = ROOT_REPOSITORY + VALID_MATCH_INDEXES[0];
+        
+        aMatchRepository.getMatchesByIndex(VALID_MATCH_INDEXES);
         
         verify(JSONMatchConverter).load(expectedPathOfFileToLoad);
     }
+    
+    @Test
+    public void whenGettingAListOfMatchesIfAMatchIsLoadedThenItIsReturned() throws FileNotFoundException{
+        doReturn(match).when(JSONMatchConverter).load(anyString());
+        aMatchRepository.getMatchesByIndex(VALID_MATCH_INDEXES);
+        reset(JSONMatchConverter);
+        
+        aMatchRepository.getMatchesByIndex(VALID_MATCH_INDEXES);
+        
+        verifyZeroInteractions(JSONMatchConverter);
+    }
+    
+    @Test
+    public void whenGettingAMatchAnyErrorsArePrintToTheErrorStream() throws FileNotFoundException{
+        doThrow(new FileNotFoundException(AN_ERROR_MESSAGE)).when(JSONMatchConverter).load(anyString());
+        
+        aMatchRepository.getMatchesByIndex(INVALID_MATCH_INDEXES);
+        
+        assertEquals(AN_ERROR_MESSAGE + "\n", errContent.toString());
+    }
 
-//    @Test
-//    public void whenGettingAllTheMatchEntriesNoEntryIsAddedForAnInvalidFileName() throws FileNotFoundException {
-//        doReturn(INVALID_FILES_NAME_IN_A_DIRECTORY).when(fileAccessor).getFilesNameInDirectory(anyString());
-//
-//        doThrow(new FileNotFoundException(FILE_NOT_FOUND_EXCEPTION_MESSAGE)).when(JSONMatchConverter)
-//                                                                            .load(anyString());
-//        Map<Integer, Match> entries = aMatchRepository.getAllLoadedEntries();
-//
-//        assertTrue(entries.isEmpty());
-//    }
-//
-//    @Test
-//    public void whenGettingAllTheMatchEntriesAllTheMatchEntriesAreReturned() throws FileNotFoundException {
-//        doReturn(VALID_FILES_NAME_IN_A_DIRECTORY).when(fileAccessor).getFilesNameInDirectory(anyString());
-//        doReturn(match).when(JSONMatchConverter).load(anyString());
-//        aMatchRepository.loadAllMatches();
-//        Map<Integer, Match> entries = aMatchRepository.getAllLoadedEntries();
-//
-//        assertFalse(entries.isEmpty());
-//    }
-//
-//    @Test
-//    public void whenTryingToGetAMatchByAValidIdAMatchIsReturned() throws FileNotFoundException {
-//        doReturn(VALID_FILES_NAME_IN_A_DIRECTORY).when(fileAccessor).getFilesNameInDirectory(anyString());
-//        doReturn(match).when(JSONMatchConverter).load(anyString());
-//        aMatchRepository.loadAllMatches();
-//        aMatchRepository.getAllLoadedEntries();
-//
-//        Match returnedMatch = aMatchRepository.getById(0);
-//        assertEquals(match, returnedMatch);
-//    }
-//
-//    @Test(expected = RepositoryException.class)
-//    public void whenTryingToGetAMatchByAnInvalidIdAnExceptionIsThrown() throws FileNotFoundException {
-//        aMatchRepository.getById(0);
-//    }
 }
