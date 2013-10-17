4e43826848ccc56d99fb80428c1fce3b2677eb73
Kevin Dion <kevin.dion.1@ulaval.ca>
2013-10-17 14:18:58 -0400

    Made test working for MatchQueryFactory

--- src/test/java/ca/ulaval/glo4003/matchCatalog/MatchQueryFactoryTest.java
+++ src/test/java/ca/ulaval/glo4003/matchCatalog/MatchQueryFactoryTest.java
@@ -2,45 +2,44 @@ package ca.ulaval.glo4003.matchCatalog;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 
 public class MatchQueryFactoryTest {
 
-    private static final String A_VALID_JSON_STRING = "{\"SPORT\":[{\"name\":\"basket\",\"value\":\"on\"}]}";
+    private static final String A_VALID_JSON_STRING = "{\"SPORT\":[{\"name\":\"basketball\",\"value\":\"on\"}]}";
     private static final MatchFilterCategories EXPECTED_FILTER_CATEGORY = MatchFilterCategories.SPORT;
-    private static final String EXPECTED_FILTER_VALUE = "filter1";
-    private static final String A_VALID_JSON_STRING_WITH_MORE_THAN_ONE_FILTER = "{\"SPORT\":[{\"name\":\"basket\",\"value\":\"on\"}],"
+    private static final String EXPECTED_FILTER_VALUE = "basketball";
+    private static final String A_VALID_JSON_STRING_WITH_MORE_THAN_ONE_FILTER = "{\"SPORT\":[{\"name\":\"basketball\",\"value\":\"on\"}],"
                                                                                 + "\"VENUE\":[{\"name\":\"tenis\",\"value\":\"on\"}]}";
     private static final MatchFilterCategories NEXT_EXPECTED_FILTER_CATEGORY = MatchFilterCategories.VENUE;
-    private static final String NEXT_EXPECTED_FILTER_VALUE = "filter1";
+    private static final String NEXT_EXPECTED_FILTER_VALUE = "tenis";
 
     private MatchQueryFactory matchQueryFactory;
 
-    @Mock
-    Query<MatchFilterCategories> aQuery;
-
     @Before
-    public void setup() {
+    public void setup() throws Exception {
         MockitoAnnotations.initMocks(this);
         matchQueryFactory = new MatchQueryFactory();
     }
 
     @Test
-    public void whenGivenAJSONStringTheFactoryCanAddAFilterToTheQuery() {
-        matchQueryFactory.create(A_VALID_JSON_STRING);
+    public void whenGivenAJSONStringTheFactoryCanAddAFilterToTheQuery() throws Exception {
+        Query<MatchFilterCategories> query = matchQueryFactory.create(A_VALID_JSON_STRING);
 
-        verify(aQuery).addFilterValue(EXPECTED_FILTER_CATEGORY, EXPECTED_FILTER_VALUE);
+        String filterValue = query.getFilterValuesForCategory(EXPECTED_FILTER_CATEGORY).get(0);
+        assertEquals(EXPECTED_FILTER_VALUE, filterValue);
     }
 
     @Test
     public void whenGivenAJSONStringAllTheFilterAreAddedToTheQuery() {
-        matchQueryFactory.create(A_VALID_JSON_STRING_WITH_MORE_THAN_ONE_FILTER);
-
-        verify(aQuery).addFilterValue(EXPECTED_FILTER_CATEGORY, EXPECTED_FILTER_VALUE);
-        verify(aQuery).addFilterValue(NEXT_EXPECTED_FILTER_CATEGORY, NEXT_EXPECTED_FILTER_VALUE);
+        Query<MatchFilterCategories> query = matchQueryFactory.create(A_VALID_JSON_STRING_WITH_MORE_THAN_ONE_FILTER);
+        
+        String filterValue = query.getFilterValuesForCategory(EXPECTED_FILTER_CATEGORY).get(0);
+        assertEquals(EXPECTED_FILTER_VALUE, filterValue);
+        
+        String nextFilterValue = query.getFilterValuesForCategory(NEXT_EXPECTED_FILTER_CATEGORY).get(0);
+        assertEquals(NEXT_EXPECTED_FILTER_VALUE, nextFilterValue);
     }
 
 }
