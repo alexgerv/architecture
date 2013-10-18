2cdbd1dd3c62d9c57c7385733be82d395d6d1eb8
Philippe Bouillon <philippe.bouillon.1@ulaval.ca>
2013-10-18 00:51:03 -0400

    MatchTest Getters Setters

--- src/main/java/ca/ulaval/glo4003/web/viewmodels/MatchViewModel.java
+++ src/main/java/ca/ulaval/glo4003/web/viewmodels/MatchViewModel.java
@@ -1,88 +1,90 @@
 package ca.ulaval.glo4003.web.viewmodels;
 
-import java.util.Date;
 import java.util.Map;
 
 import ca.ulaval.glo4003.model.Sex;
 
 public class MatchViewModel {
-	public String matchIdentifier;
-    public String sport;
-    public String venue;
-    public String date;
-	public String homeTeam;
-	public String visitorTeam;
-	public Sex sex;
-	public Map<String, Integer> availableTicketsBySection;
-	public int totalNumberOfAvailableTickets;
-	
-	public String getMatchIdentifier() {
-		return matchIdentifier;
-	}
-    
-	public String getSport() {
-    	return sport;
+
+    private String matchIdentifier;
+    private String sport;
+    private String venue;
+    private String date;
+    private String homeTeam;
+    private String visitorTeam;
+    private Sex sex;
+    private Map<String, Integer> availableTicketsBySection;
+    private int totalNumberOfAvailableTickets;
+
+    public String getMatchIdentifier() {
+        return matchIdentifier;
     }
-	
+
+    public void setMatchIdentifier(String identifier) {
+        this.matchIdentifier = identifier;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
     public void setSport(String sport) {
-    	this.sport = sport;
+        this.sport = sport;
     }
-    
+
     public String getVenue() {
-    	return venue;
+        return venue;
     }
-    
+
     public void setVenue(String venue) {
-    	this.venue = venue;
+        this.venue = venue;
     }
-    
+
     public String getDate() {
-		return date;
-	}
-    
-	public void setDate(String date) {
-		this.date = date;
-	}
-	
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
     public String getHomeTeam() {
-		return homeTeam;
-	}
-    
-	public void setHomeTeam(String homeTeam) {
-		this.homeTeam = homeTeam;
-	}
-	
+        return homeTeam;
+    }
+
+    public void setHomeTeam(String homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
     public String getVisitorTeam() {
-		return visitorTeam;
-	}
-    
-	public void setVisitorTeam(String visitorTeam) {
-		this.visitorTeam = visitorTeam;
-	}
-	
+        return visitorTeam;
+    }
+
+    public void setVisitorTeam(String visitorTeam) {
+        this.visitorTeam = visitorTeam;
+    }
+
     public Sex getSex() {
         return sex;
     }
-    
+
     public void setSex(Sex sex) {
         this.sex = sex;
     }
-	
-	public Map<String, Integer> getAvailableTicketsBySection() {
-		return availableTicketsBySection;
-	}
 
-	public void setAvailableTicketsBySection(
-			Map<String, Integer> availableTicketsBySection) {
-		this.availableTicketsBySection = availableTicketsBySection;
-	}
-	
-	public int getTotalNumberOfAvailableTickets() {
-		return totalNumberOfAvailableTickets;
-	}
+    public Map<String, Integer> getAvailableTicketsBySection() {
+        return availableTicketsBySection;
+    }
 
-	public void setTotalNumberOfAvailableTickets(int totalNumberOfAvailableTickets) {
-		this.totalNumberOfAvailableTickets = totalNumberOfAvailableTickets;
-	}
+    public void setAvailableTicketsBySection(Map<String, Integer> availableTicketsBySection) {
+        this.availableTicketsBySection = availableTicketsBySection;
+    }
+
+    public int getTotalNumberOfAvailableTickets() {
+        return totalNumberOfAvailableTickets;
+    }
+
+    public void setTotalNumberOfAvailableTickets(int totalNumberOfAvailableTickets) {
+        this.totalNumberOfAvailableTickets = totalNumberOfAvailableTickets;
+    }
 }
-
