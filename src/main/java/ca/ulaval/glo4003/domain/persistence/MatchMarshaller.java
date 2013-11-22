b12d4014ba4a256a49564759187450df7b006f19
Simon Gourdeau <simon.gourdeau@hotmail.com>
2013-11-22 12:35:09 -0500

    deleted package domain.persistence

--- src/main/java/ca/ulaval/glo4003/domain/persistence/MatchMarshaller.java
+++
@@ -1,6 +0,0 @@
-package ca.ulaval.glo4003.domain.persistence;
-
-import ca.ulaval.glo4003.domain.match.Match;
-
-public interface MatchMarshaller extends Marshaller<Match> {
-}
--- src/main/java/ca/ulaval/glo4003/domain/persistence/UserMarshaller.java
+++
@@ -1,6 +0,0 @@
-package ca.ulaval.glo4003.domain.persistence;
-
-import ca.ulaval.glo4003.domain.user.User;
-
-public interface UserMarshaller extends Marshaller<User>{
-}
