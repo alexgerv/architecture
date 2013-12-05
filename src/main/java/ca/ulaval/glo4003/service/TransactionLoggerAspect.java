94d899a9ec21689f4676da6dc340fe14938bbde0
Alexandre Girard <girarda.92@gmail.com>
2013-12-05 16:11:53 -0500

    Change to Spring AOP

---
+++ src/main/java/ca/ulaval/glo4003/service/TransactionLoggerAspect.java
@@ -0,0 +1,36 @@
+package ca.ulaval.glo4003.service;
+
+import java.util.List;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+import ca.ulaval.glo4003.domain.match.Ticket;
+
+@Aspect
+public class TransactionLoggerAspect {
+
+    @Pointcut("execution(* *..TransactionManager.processTransaction*(..))")
+    private void ticketPurchase() {}
+
+    @AfterReturning(pointcut = "execution(* *..TransactionManager.processTransaction*(..))",
+                    returning = "transactionID")
+    public void logTransaction(JoinPoint joinpoint, long transactionID) {
+        List<Ticket> tickets = (List<Ticket>) joinpoint.getArgs()[2];
+
+        Logger logger = LogManager.getLogger("transactionLogger");
+        logger.info("Transaction: " + transactionID + " - Sold tickets:" + ticketListToString(tickets));
+    }
+
+    private String ticketListToString(List<Ticket> tickets) {
+        String str = "";
+        for (Ticket ticket : tickets) {
+            str = str + "ID: " + ticket.getID() + ",";
+        }
+        return str.substring(0, str.length() - 1);
+    }
+}
