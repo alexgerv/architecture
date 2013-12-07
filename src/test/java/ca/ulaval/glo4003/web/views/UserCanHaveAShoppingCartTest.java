cc0a4e1bcb9a4254b072a575d5dd5d875e587b36
Alexandre Girard <girarda.92@gmail.com>
2013-12-06 20:22:44 -0500

    Add Cart acceptance tests.

---
+++ src/test/java/ca/ulaval/glo4003/web/views/UserCanHaveAShoppingCartTest.java
@@ -0,0 +1,90 @@
+package ca.ulaval.glo4003.web.views;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ca.ulaval.glo4003.testFixture.TestFixture;
+
+public class UserCanHaveAShoppingCartTest {
+
+    private static final String A_TICKET_QUANTITY = "5";
+    private static final String ANOTHER_TICKET_QUANTITY = "3";
+
+    private TestFixture fixture;
+
+    @Before
+    public void setup() {
+        fixture = new TestFixture();
+        fixture.init();
+    }
+
+    @After
+    public void tearDown() {
+        fixture.close();
+    }
+
+    @Test
+    public void userCanAddTicketsToItsCart() {
+
+        fixture.goOnHomePage();
+
+        fixture.goOnLoginPage();
+
+        fixture.logInWithRightCredentials();
+
+        fixture.navigateToMatchDetails();
+
+        fixture.chooseASectionInMatchDetails();
+
+        fixture.selectATicketQuantityForCurrentSection(A_TICKET_QUANTITY);
+
+        fixture.addSelectedTicketsToCart();
+
+        int ticketQuantity = fixture.getFirstTicketTypeQuantityInCart();
+        assertEquals(Integer.parseInt(A_TICKET_QUANTITY), ticketQuantity);
+    }
+
+    @Test
+    public void usersCartIsEmptiedWhenHeLogsOut() {
+
+        fixture.goOnHomePage();
+        fixture.goOnLoginPage();
+        fixture.logInWithRightCredentials();
+        fixture.navigateToMatchDetails();
+        fixture.chooseASectionInMatchDetails();
+        fixture.selectATicketQuantityForCurrentSection(A_TICKET_QUANTITY);
+        fixture.addSelectedTicketsToCart();
+
+        fixture.logOut();
+        fixture.logInWithRightCredentials();
+
+        fixture.goOnCartPage();
+
+        fixture.assertTheCartIsEmpty();
+    }
+
+    @Test
+    public void userCanAddMultipleTypesOfTicketsToItsCart() {
+
+        fixture.goOnHomePage();
+
+        fixture.goOnLoginPage();
+
+        fixture.logInWithRightCredentials();
+
+        fixture.navigateToMatchDetails();
+        fixture.chooseASectionInMatchDetails();
+        fixture.selectATicketQuantityForCurrentSection(A_TICKET_QUANTITY);
+        fixture.addSelectedTicketsToCart();
+
+        fixture.navigateToMatchDetails();
+        fixture.chooseAnotherSectionInMatchDetails();
+        fixture.selectATicketQuantityForCurrentSection(ANOTHER_TICKET_QUANTITY);
+        fixture.addSelectedTicketsToCart();
+
+        fixture.assertTwoTypesOfTicketsInCart();
+    }
+}
