92ae587b96a1ce9fe1503edcf8f432256939042f
Alexandre Girard <girarda.92@gmail.com>
2013-12-10 14:04:11 -0500

    Add comment in MimeMessageBuilderTest.

--- src/test/java/ca/ulaval/glo4003/service/mailsender/MimeMessageBuilderTest.java
+++ src/test/java/ca/ulaval/glo4003/service/mailsender/MimeMessageBuilderTest.java
@@ -1,33 +1,24 @@
 package ca.ulaval.glo4003.service.mailsender;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertTrue;
-
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-
-import static org.mockito.Mockito.doThrow;
-
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 public class MimeMessageBuilderTest {
 
+    // These are not unit tests as we could not mock the javax.mail objects
+    // properly
+
     private static final String A_SIGNATURE_LOGO = "";
     private static final String A_SIGNATURE_ID = "";
-    
+
     private static final String INVALID_DESTINATION = "é";
-    
+
     private static final String A_DEFAULT_SENDER = "userglo4003@gmail.com";
     private static final String A_PERSONAL_SENDER = "userglo4003";
     private static final String A_DESTINATION = "dest@gmail.com";
@@ -39,15 +30,13 @@ public class MimeMessageBuilderTest {
     private static final String OTHER_DESTINATION = "dest2@gmail.com";
     private static final String OTHER_SUBJECT = "Testing seccond message";
     private static final String OTHER_BODY = "Message body for other message";
-    
+
     private JavaMailSenderImpl mailServer = new JavaMailSenderImpl();
 
     private MimeMessageBuilder mimeMessageBuilder;
-    
+
     @Before
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        
         mimeMessageBuilder = new MimeMessageBuilder();
 
         mimeMessageBuilder.setDefaultSender(A_DEFAULT_SENDER);
@@ -62,13 +51,13 @@ public class MimeMessageBuilderTest {
     @Test(expected = MessageBuilderException.class)
     public void aMessageBuilderExceptionIsThrownWhenAnEmailAddressIsInvalid() {
         mimeMessageBuilder.setDestination(INVALID_DESTINATION);
-        
+
         mimeMessageBuilder.build(mailServer);
     }
-    
+
     @Test
     public void canBuildTwoDifferentMimeMessage() throws MessagingException {
-        
+
         MimeMessage firstMessage = mimeMessageBuilder.build(mailServer);
 
         mimeMessageBuilder.setDefaultSender(OTHER_DEFAULT_SENDER);
